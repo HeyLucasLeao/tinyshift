bdc6664d2a65e4c1f9d68d1a7635b6766ea21f53
Lucas Leão <heylucasleao@gmail.com>
2025-01-04 22:27:39 -0400

    🔥 remove unnecessary funcs

--- src/scoring.py
+++ src/scoring.py
@@ -1,29 +1,5 @@
 import numpy as np
 from scipy.stats import norm
-from sklearn.metrics import f1_score
-import pandas as pd
-
-
-def metric_by_time_period(
-    df, target_col, prediction_col, timestamp_col, period, metric=f1_score
-):
-    """
-    Calculate a specified metric for each time period in the DataFrame.
-
-    Parameters:
-    - df: pandas DataFrame containing the data.
-    - period: The time period for grouping data (e.g., 'W' for weeks, 'M' for months).
-    - target_col: The name of the column containing the actual target values.
-    - prediction_col: The name of the column containing the predicted values.
-    - metric: A function to compute the metric (e.g., f1_score).
-
-    Returns:
-    - pandas DataFrame: A DataFrame with the metric calculated for each time period.
-    """
-    grouped = df.groupby(pd.Grouper(key=timestamp_col, freq=period)).apply(
-        lambda x: metric(x[target_col], x[prediction_col])
-    )
-    return grouped.reset_index(name="metric")
 
 
 def bootstrapping_bca(
@@ -103,92 +79,6 @@ def bootstrapping_bca(
     upper_bound = np.quantile(sample_statistics, alpha_upper)
 
     return lower_bound, upper_bound
-
-
-def reference_metrics_by_period(
-    df,
-    period,
-    target_col,
-    prediction_col,
-    score_method=f1_score,
-    statistic=np.mean,
-    confidence_level=0.997,
-    n_resamples=1000,
-    random_state=42,
-):
-    """
-    Calculate reference metrics for a DataFrame grouped by a specified
-    time period, including confidence intervals and thresholds.
-
-    Parameters:
-    ----------
-    - df : pd.DataFrame
-        The input DataFrame containing the data.
-    - period : str
-        The time period for grouping (e.g., 'W' for weeks, 'M' for months).
-    - target_col : str
-        The column name containing the true labels or observed values.
-    - prediction_col : str
-        The column name containing the predicted values.
-    - score_method : function
-        The metric function to evaluate the model's performance (e.g., mean squared error, accuracy score).
-    - statistic : function
-        The statistical function to apply to the metrics (e.g., np.mean, np.median).
-    - confidence_level : float, optional (default=0.997)
-        The confidence level for the bootstrapped confidence interval (e.g., 0.95 for 95% confidence level).
-    - n_resamples : int, optional (default=1000)
-        The number of bootstrap resamples to perform for confidence interval calculation.
-    - random_state : int, optional (default=42)
-        Random seed for reproducibility.
-
-    Returns:
-    -------
-    - dict
-        A dictionary containing the following keys:
-        - "ci_lower" : float
-            The lower bound of the bias-corrected and accelerated (BCa) bootstrap confidence interval.
-        - "ci_upper" : float
-            The upper bound of the BCa bootstrap confidence interval.
-        - "mean" : float
-            The mean of the calculated metric across the specified period.
-        - "lower_threshold" : float
-            The lower threshold for anomaly detection, calculated as mean - 3 * standard deviation.
-        - "upper_threshold" : float
-            The upper threshold for anomaly detection, calculated as mean + 3 * standard deviation.
-
-    Notes:
-    ------
-    - The confidence interval is calculated using the BCa two-sided bootstrap method.
-    - The thresholds (lower and upper) are based on a 3-sigma rule assuming normal distribution.
-
-    """
-
-    metrics_by_period = metric_by_time_period(
-        df, period, target_col, prediction_col, score_method
-    )
-
-    ci_lower, ci_upper = bootstrapping_bca(
-        metrics_by_period["metric"],
-        confidence_level,
-        statistic,
-        n_resamples,
-        random_state,
-    )
-
-    estimated_mean_statistic = np.mean(metrics_by_period["metric"])
-    std_deviation = metrics_by_period["metric"].std()
-    lower_threshold = estimated_mean_statistic - (std_deviation * 3)
-    upper_threshold = estimated_mean_statistic + (std_deviation * 3)
-
-    results = {
-        "ci_lower": ci_lower,
-        "ci_upper": ci_upper,
-        "mean": estimated_mean_statistic,
-        "lower_threshold": lower_threshold,
-        "upper_threshold": upper_threshold,
-    }
-
-    return results
 
 
 def deviation_threshold(df):
