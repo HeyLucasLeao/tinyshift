83ddad7ce381f29a65988ba38bcd4bbe17f59824
Lucas Leão <heylucasleao@gmail.com>
2025-07-26 23:32:37 -0300

    ♻️ refactor parameter name from 'data' to 'X' in StatisticalInterval methods for consistency

--- tinyshift/stats/statistical_interval.py
+++ tinyshift/stats/statistical_interval.py
@@ -12,7 +12,7 @@ class StatisticalInterval:
 
     @staticmethod
     def calculate_interval(
-        data: np.ndarray,
+        X: np.ndarray,
         center: Callable,
         spread: Callable,
         factor: float = 3,
@@ -22,49 +22,49 @@ class StatisticalInterval:
         if not callable(center) or not callable(spread):
             raise ValueError("center and spread must be callable functions")
 
-        center_value = center(data)
-        spread_value = spread(data)
+        center_value = center(X)
+        spread_value = spread(X)
         lower_bound = center_value - factor * spread_value
         upper_bound = center_value + factor * spread_value
         return lower_bound, upper_bound
 
     @staticmethod
-    def iqr_interval(data: np.ndarray) -> Tuple[float, float]:
+    def iqr_interval(X: np.ndarray) -> Tuple[float, float]:
         """Calculates interval using IQR and median with a default factor of 1.5."""
 
         def iqr(x):
             q75, q25 = np.percentile(x, [75, 25])
             return q75 - q25
 
-        return StatisticalInterval.calculate_interval(data, np.median, iqr, factor=1.5)
+        return StatisticalInterval.calculate_interval(X, np.median, iqr, factor=1.5)
 
     @staticmethod
-    def stddev_interval(data: np.ndarray) -> Tuple[float, float]:
+    def stddev_interval(X: np.ndarray) -> Tuple[float, float]:
         """Calculates interval using mean and standard deviation."""
-        return StatisticalInterval.calculate_interval(data, np.mean, np.std)
+        return StatisticalInterval.calculate_interval(X, np.mean, np.std)
 
     @staticmethod
-    def mad_interval(data: np.ndarray) -> Tuple[float, float]:
+    def mad_interval(X: np.ndarray) -> Tuple[float, float]:
         """Calculates interval using Median Absolute Deviation (MAD)."""
         mad = lambda x: np.median(np.abs(x - np.median(x)))
-        return StatisticalInterval.calculate_interval(data, np.median, mad)
+        return StatisticalInterval.calculate_interval(X, np.median, mad)
 
     @staticmethod
     def quantile_interval(
-        data: np.ndarray, lower: float, upper: float
+        X: np.ndarray, lower: float, upper: float
     ) -> Tuple[float, float]:
         """Calculates interval using quantiles."""
         lower_bound = (
-            np.quantile(data, lower, method="higher") if lower is not None else None
+            np.quantile(X, lower, method="higher") if lower is not None else None
         )
         upper_bound = (
-            np.quantile(data, upper, method="higher") if upper is not None else None
+            np.quantile(X, upper, method="higher") if upper is not None else None
         )
         return (lower_bound, upper_bound)
 
     @staticmethod
     def compute_interval(
-        data: np.ndarray,
+        X: np.ndarray,
         method: Union[str, Callable, Tuple[float]],
     ) -> Tuple[float, float]:
         """
@@ -82,24 +82,24 @@ class StatisticalInterval:
         Returns:
             Tuple[float, float]: Lower and upper bounds.
         """
-        data = np.asarray(data)
+        X = np.asarray(X)
         if isinstance(method, str):
             if method == "stddev":
-                lower_bound, upper_bound = StatisticalInterval.stddev_interval(data)
+                lower_bound, upper_bound = StatisticalInterval.stddev_interval(X)
             elif method == "mad":
-                lower_bound, upper_bound = StatisticalInterval.mad_interval(data)
+                lower_bound, upper_bound = StatisticalInterval.mad_interval(X)
             elif method == "iqr":
-                lower_bound, upper_bound = StatisticalInterval.iqr_interval(data)
+                lower_bound, upper_bound = StatisticalInterval.iqr_interval(X)
             else:
                 raise ValueError(f"Unsupported method: {method}")
         elif callable(method):
-            lower_bound, upper_bound = StatisticalInterval.custom_interval(data, method)
+            lower_bound, upper_bound = StatisticalInterval.custom_interval(X, method)
         elif isinstance(method, tuple) and len(method) == 2:
             lower_bound, upper_bound = method
         elif isinstance(method, tuple) and len(method) == 3 and method[0] == "quantile":
             lower, upper = method[1], method[2]
             lower_bound, upper_bound = StatisticalInterval.quantile_interval(
-                data, lower, upper
+                X, lower, upper
             )
         else:
             raise ValueError("Invalid method specification.")
