14ac934696809a9e8c68b0e3f91db36137af52fe
Lucas Leão <heylucasleao@gmail.com>
2025-08-31 12:48:51 -0300

    🚚 moving files into association_mining and adding Bayesian Confidence

--- tinyshift/modelling/__init__.py
+++ tinyshift/modelling/__init__.py
@@ -6,4 +6,3 @@
 from .multicollinearity import filter_features_by_vif
 from .residualizer import FeatureResidualizer
 from .scaler import RobustGaussianScaler
-from .association_rule.analyzer import TransactionAnalyzer
